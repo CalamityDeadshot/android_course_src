3d9e969de8cdfbe122b1aa493dbac45e6bb4c2f9
CalamityDeadshot <semyon.pyankov@gmail.com>
2022-04-18 03:25:20 +0300

    More examples

--- src/main.kt
+++ src/main.kt
@@ -1,18 +1,21 @@
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.util.ArrayList
+import kotlinx.coroutines.*
 import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 
 fun main() {
-    coroutines()
-//    threads()
+    val suspendingTime = measureTimeMillis {
+        suspendingCalls()
+    }
+    val badConcurrencyTime = measureTimeMillis {
+        badConcurrency()
+    }
+    val asyncAwaitTime = measureTimeMillis {
+        asyncAwait()
+    }
+    println("Synchronized time is $suspendingTime ms")
+    println("Concurrent time is $badConcurrencyTime ms")
+    println("Async-await time is $asyncAwaitTime ms")
 }
 
 fun coroutines() = runBlocking {
@@ -31,4 +34,44 @@ fun threads() {
             print("$it ")
         }
     }
+}
+
+fun suspendingCalls() = runBlocking {
+    launch {
+        val data1 = getSomeData1()
+        val data2 = getSomeData2()
+        println("Data 1: $data1, data2: $data2")
+    }
+}
+
+fun badConcurrency() = runBlocking {
+    var data1: String? = null
+    var data2: String? = null
+
+    val job1 = launch {
+        data1 = getSomeData1()
+    }
+    val job2 = launch {
+        data2 = getSomeData1()
+    }
+
+    job1.join()
+    job2.join()
+    println("Data 1: $data1, data2: $data2")
+}
+
+fun asyncAwait() = runBlocking {
+    val data1 = async { getSomeData1() }
+    val data2 = async { getSomeData2() }
+
+    println("Data 1: ${data1.await()}, data2: ${data2.await()}")
+}
+
+suspend fun getSomeData1(): String {
+    delay(2000)
+    return "Data 1"
+}
+suspend fun getSomeData2(): String {
+    delay(2000)
+    return "Data 2"
 }
